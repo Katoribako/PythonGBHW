25a64baa752871df8237a6267dace2fc272d0c83
Katoribako <64684310+Katoribako@users.noreply.github.com>
2023-04-23 19:05:45 +0300

    Added Seminar9 with tasks from sem6

--- dascriptor_2.py
+++ dascriptor_2.py
@@ -1,32 +1,45 @@
-'''
-Реализовать дескрипторы для любых двух классов
-'''
+# Реализовать класс Road (дорога), в котором определить защищенные атрибуты:
+# length (длина в метрах), width (ширина в метрах).
+# Значения данных атрибутов должны передаваться при создании экземпляра класса.
+# Реализовать публичный метод расчета массы асфальта, необходимого для покрытия
+# всего дорожного полотна.
+# Использовать формулу: длина * ширина * масса асфальта для покрытия одного кв
+# метра дороги асфальтом, толщиной в 1 см * число м толщины полотна.
+# Массу и толщину сделать публичными атрибутами.
+# Проверить работу метода.
+# Например: 20м*5000м*25кг*0.05м = 125000 кг = 125 т
 
-
-class CorrectAttr:
-    def __set_name__(self, owner, parameter):
-        self.parameter = parameter
+class NumVerificator:
+    def __set_name__(self, owner, my_attr):
+        self.parameter = my_attr
 
     def __set__(self, instance, value):
+        if type(value) != int and type(value) != float:
+            raise NameError("Please input an integer number that bigger than"
+                            " zero")
         if value <= 0:
-            raise ValueError("Please input a number is bigger than zero")
+            raise ValueError("Please input an integer number that bigger than"
+                             " zero")
         instance.__dict__[self.parameter] = value
 
 
-class Room:
-    _length = CorrectAttr()
-    _width = CorrectAttr()
-    _height = CorrectAttr()
+class Road:
+    _asph_len = NumVerificator()
+    _asph_width = NumVerificator()
+    _weight_per_sm = NumVerificator()
+    _thickness = NumVerificator()
 
-    def __init__(self, length, width, height):
-        self._length = length
-        self._width = width
-        self._height = height
+    def __init__(self, asph_len, asph_width, weight_per_sm, thickness):
+        self._asph_len = asph_len
+        self._asph_width = asph_width
+        self._weight_per_sm = weight_per_sm
+        self._thickness = thickness
 
-    def room_square(self):
-        square = self._length * self._width * self._height
-        return int(square)
+    def asph_weigth(self):
+        thickness = 0.05
+        return self._asph_len * self._asph_width * self._thickness \
+            * self._weight_per_sm
 
 
-room1 = Room(10, 10, 10)
-print(f"The square of current room is {room1.room_square()}")
+road1 = Road(12, 20, 20.4, 12)
+print(f"weight of road is {road1.asph_weigth()}")
--- descriptor_1.py
+++ descriptor_1.py
@@ -1,34 +1,42 @@
-'''
-Реализовать дескрипторы для любых двух классов
-'''
+# Создать класс TrafficLight (светофор)
+# и определить у него один приватный атрибут color (цвет) и
+# публичный метод running (запуск).
+# В рамках метода running реализовать переключение светофора в режимы:
+# красный, желтый, зеленый. Продолжительность первого состояния (красный)
+# составляет 7 секунд, второго (желтый) — 2 секунды, третьего (зеленый) —
+# на ваше усмотрение.
+# Для имитации "горения" каждого цвета испольщуйте ф-цию sleep модуля time
+# Переключение между режимами должно осуществляться только
+# в указанном порядке (красный, желтый, зеленый).
+# Проверить работу примера, создав экземпляр и вызвав описанный метод.
+
+import time
 
 
-class TrueNum:
-    def __set_name__(self, owner, parameter):
-        self.parameter = parameter
+class ArrayDetector:
+    def __set_name__(self, owner, my_attr):
+        self.parameter = my_attr
 
     def __set__(self, instance, value):
-        if value <= 0:
-            raise ValueError("Please input an integer number that bigger than"
-                             " zero")
-        if value % 1 != 0:
-            raise ValueError("Please input an integer number that bigger than"
-                             " zero")
+        if type(value) != list:
+            raise ValueError("Please input a data with using a lists")
         instance.__dict__[self.parameter] = value
 
 
-class House:
-    _floors_num = TrueNum()
-    _apart_per_floor = TrueNum()
+class TrafficLight:
+    _color_list = ArrayDetector()
+    _waiting_time = ArrayDetector()
 
-    def __init__(self, num_of_floors, apart_per_floor):
-        self._floors_num = num_of_floors
-        self._apart_per_floor = apart_per_floor
+    def __init__(self, color_list, waiting_list):
+        self._color_list = color_list
+        self._waiting_time = waiting_list
 
-    def apart_amount(self):
-        amount = self._floors_num * self._apart_per_floor
-        return amount
+    def working_process(self):
+        for color, wait in zip(self._color_list, self._waiting_time):
+            print(f"{color} цвет будет работать "
+                  f"{wait} секунд ")
+            time.sleep(int(wait))
 
 
-house_1 = House(7, 7)
-print(f"Amount of apartaments in this house is {house_1.apart_amount()}")
+tl = TrafficLight(['красный', 'зелёный'], [7, 5])
+tl.working_process()
